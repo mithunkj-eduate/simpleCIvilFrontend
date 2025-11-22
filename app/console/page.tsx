f99db4b5bd1a9f242a86d5aac22bae53f4045cc8
mithunkj-eduate <mithunkj@myeduate.com>
2025-11-22 18:46:27 +0530

    changes

--- app/console/page.tsx
+++ app/console/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Navbar from "@/components/commen/Navbar";
 import { AppContext } from "@/context/context";
 import { LicenseTypes } from "@/utils/enum.types";
@@ -17,7 +17,16 @@ const DashBord = () => {
   return (
     <>
       <Navbar NavType={LicenseTypes.ADMIN} />
-      <div className="mt-24">DashBord</div>
+
+      <div className="flex-1 min-h-screen flex flex-col justify-between">
+        <div className="mx-auto max-w-7xl px-4  py-8 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between pb-4">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+              DashBord
+            </h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
